dd51bcb276f0a5cf5eb640bc7cde07008338165c
phaneth07 <phanethsorn@gmail.com>
2020-10-10 14:13:47 +0700

    push to deploy

---
+++ README.md
@@ -0,0 +1,7 @@
+composer install <br>
+php artisan key:generate <br>
+php artisan migrate <br>
+php artisan db:seed <br>
+php artisan apiato:permissions:toRole admin <br>
+php artisan passport:install (for install oauth_private.key)<br>
+php artisan passport:client --password (for generate client ID client secrete)
---
+++ app/Containers/User/Data/Migrations/2000_01_01_000001_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('password')->nullable();
+            $table->string('plan')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('status')->nullable();
+            $table->boolean('confirmed')->default(false);
+            $table->string('gender')->nullable();
+            $table->string('birth')->nullable();
+            $table->string('device')->nullable();
+            $table->string('platform')->nullable();
+            $table->boolean('is_client')->default(false);
+
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
